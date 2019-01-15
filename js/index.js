37718abefed92f083dc6d81803b4da4d3db9de94
ZeldOcarina <mattia.r86@gmail.com>
2019-01-15 15:29:27 +0100

    Add first countUp animations

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+$('.counter-header').counterUp({
+    delay: 50,
+    time: 1500
+});
+
+setTimeout(function () {
+    $('.counter-immobili-venduti').counterUp({
+        delay: 10,
+        offset: 100,
+        time: 1500
+    });
+}, 1000);
+
+
+$('.counter-promozioni').counterUp({
+    delay: 10,
+    time: 2000
+});
+
