a2a0b6642dba58b74629f2862000951010e3d460
ZeldOcarina <mattia.r86@gmail.com>
2019-01-16 10:36:10 +0100

    Complete jQuery animations

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+//DOM Animations
+// Counter up effects
+
 $('.counter-header').counterUp({
-    delay: 50,
-    time: 1500
+    delay: 40,
+    time: 1300
 });
 
 setTimeout(function () {
@@ -14,6 +17,37 @@ setTimeout(function () {
 
 $('.counter-promozioni').counterUp({
     delay: 10,
-    time: 2000
+    time: 1000
 });
+
+// Bounce Effects
+
+var waypoint = new Waypoint({
+    element: document.getElementById('team'),
+    handler: function (direction) {
+        $(".team-word").effect("bounce", { times: 6 }, "slow");
+    },
+    offset: "50%"
+});
+
+var waypoint2 = new Waypoint({
+    element: document.getElementById('commerciale'),
+    handler: function (direction) {
+        $(".commerciale-word").effect("bounce", { times: 6 }, "slow")
+    },
+    offset: "30%"
+});
+
+//Highlight effect on CTA
+
+var waypoint3 = new Waypoint({
+    element: document.getElementById('CTA'),
+    handler: function (direction) {
+        $(".contattaci-subito").effect("highlight");
+        $(".contattaci-subito").effect("highlight");
+    },
+    offset: 400
+});
+
+
 
